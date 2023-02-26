66652248746bca89d003498db694dbe467c25fc7
Rafael <velofuri@gmail.com>
2023-02-26 11:46:35 -0300

    Adicionado transacoes ao metodo cancelaPessoa

--- api/routes/pessoasRoute.js
+++ api/routes/pessoasRoute.js
@@ -11,12 +11,13 @@ router.get('/pessoas/:estudanteId/matricula', PessoaController.pegaMatriculas)
 router.get('/pessoas/matricula/:turmaId/confirmadas', PessoaController.pegaMatriculasPorTurma);
 router.get('/pessoas/matricula/lotada', PessoaController.pegaTurmasLotadas);
 router.post('/pessoas', PessoaController.criaPessoa);
-router.put('/pessoas/:id', PessoaController.atualizaPessoa);
-router.delete('/pessoas/:id', PessoaController.apagaPessoa);
-router.post('/pessoas/:id/restaura', PessoaController.restauraPessoa);
 router.post('/pessoas/:estudanteId/matricula', PessoaController.criaMatricula);
-router.put('/pessoas/:estudanteId/matricula/:matriculaId', PessoaController.atualizaMatricula);
-router.delete('/pessoas/:estudanteId/matricula/:matriculaId', PessoaController.apagaMatricula);
+router.post('/pessoas/:id/restaura', PessoaController.restauraPessoa);
 router.post('/pessoas/:estudanteId/matricula/:matriculaId/restaura', PessoaController.restauraMatricula);
+router.post('/pessoas/:estudanteId/cancela', PessoaController.cancelaPessoa);
+router.put('/pessoas/:id', PessoaController.atualizaPessoa);
+router.put('/pessoas/:estudanteId/matricula/:matriculaId', PessoaController.atualizaMatricula);
+router.delete('/pessoas/:id', PessoaController.apagaPessoa);
+router.delete('/pessoas/:estudanteId/matricula/:matriculaId', PessoaController.apagaMatricula);
 
 module.exports = router;
